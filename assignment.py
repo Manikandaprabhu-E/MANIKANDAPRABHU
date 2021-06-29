9994b27a0d030e78d922d6ddf3475643310133f8
Manikandaprabhu-E <49081212+Manikandaprabhu-E@users.noreply.github.com>
2021-06-29 23:43:34 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,11 @@
+def reverse(string):
+    string = string[::-1]
+    return string
+
+s = "Manikandaprabhu"
+
+print("The orginal string is: ", end= "")
+print(s)
+
+print("The reverse string is: ", end="")
+print(reverse(s))
